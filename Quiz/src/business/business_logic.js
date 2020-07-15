4121ad1c353d4575df93520100c2a53404e184cf
eleonora-ollaik <eleonora-ollaik@users.noreply.github.com>
2020-07-14 22:57:29 -0600

    business logic updated

---
+++ Quiz/src/business/business_logic.js
@@ -0,0 +1,131 @@
+class QuestionsAndAnswers {
+
+    // static lastKey = 0;
+
+    constructor () {
+        this.category = null;
+        this.type = null;
+        this.question = null;
+        this.number_of_correct_entries = 0;
+        this.number_of_incorrect_entries = 0;
+        this.correct_answers = [];
+        this.incorrect_answers = [];        
+    }
+
+    assignType (type, optionlist) {
+        for (let i = 0; i < optionlist.length; i++){
+            if (type === optionlist[i]) {
+                this.type = type;
+            }
+        }
+    }
+
+    assignCorrectAnsw (answers) {
+
+        let types = [
+            {'type': 'multiple choice', 'min': 1, 'max': 1,},
+            {'type': 'true or false', 'min': 1, 'max': 1,},
+            {'type': 'open ended question', 'min': 1, 'max': 5,}
+        ]        
+
+        for (let i=0; i < types.length; i++) {
+            if(this.type === types[i].type) {
+                if(types[i].min <= answers.length && answers.length <= types[i].max) {
+                    this.correct_answers = answers;
+                } 
+
+            }
+        }
+    }
+        assignCorrectAnsw (answers) {
+
+            let types = [
+                {'type': 'multiple choice', 'min': 1, 'max': 1,},
+                {'type': 'true or false', 'min': 1, 'max': 1,},
+                {'type': 'open ended question', 'min': 1, 'max': 5,}
+            ]        
+    
+            for (let i=0; i < types.length; i++) {
+                if(this.type === types[i].type) {
+                    if(types[i].min <= answers.length && answers.length <= types[i].max) {
+                        this.correct_answers = answers;
+                    } 
+                    
+                }
+            }
+         
+    
+        // if (this.type === 'multiple choice') {
+        //     if (answers.length === 1) {
+        //         this.correct_answers = answers;
+        //     }
+            
+        // } else if (this.type === 'true or false') {
+        //     if (answers.length === 1) {
+        //         this.correct_answers = answers;
+        //     }
+        // } else if (this.type === 'open ended question') {
+        //     if (answers.length >= 1) {
+        //         this.correct_answers = answers;
+        //     }        
+    }
+
+    calculate_difficulty() {
+        let total_answers = (this.correct_answers + this.incorrect_answers);        
+        let acurracy = total_answers===0 ? 0: this.correct_answers / total_answers;
+        
+        let comlexityInfo = [
+            {'lowest': 0.76, 'highest': 1, 'level': 'easy'},
+            {'lowest': 0.33, 'highest': 0.76, 'level': 'normal'},
+            {'lowest': 0, 'highest': 0.33, 'level': 'hard'}
+        ]        
+
+        let result = 'Undetermined';
+
+        for (let i=0; i < comlexityInfo.length; i++) {            
+            if (acurracy > comlexityInfo[i].lowest && acurracy <= comlexityInfo[i].highest ) {
+                result = comlexityInfo[i].level;
+            }               
+        }
+        return result;
+    }
+
+
+    // Generate new key (UUID) for new questions and answers object
+    // newKey() {
+    //     return ++QuestionsAndAnswers.lastKey;
+    // }    
+}
+
+class Quiz {
+    static lastKey = 0;
+
+    constructor(name, theme) {
+        this.name = name;
+        this.theme = theme;
+        this.QuestionsAndAnswers = {};
+    }
+
+    // Generate new key (UUID) for new questions and answers object
+    newKey() {
+        return ++Quiz.lastKey;
+    }
+
+    addQuestionsAndAnswers () {
+        const key = this.newKey();
+
+        this.QuestionsAndAnswers[key] = new QuestionsAndAnswers();
+
+        return key;
+    }    
+
+    deleteQuestionsAndAnswers (key) {
+        delete this.QuestionsAndAnswers[key];
+    }
+
+    getQuestionAndAnswers (key) {
+        return this.QuestionsAndAnswers[key];
+    }
+}
+
+export default {QuestionsAndAnswers, Quiz}
