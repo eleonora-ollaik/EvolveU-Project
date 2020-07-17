721f58f1f233de4a916c20abf24a753634a7aafd
Boris Kwok <bkwok123@gmail.com>
2020-07-16 20:00:35 -0600

    Edit Feature Draft 2

--- Quiz/src/pages/createQuizPage/QuestionComp.js
+++ Quiz/src/pages/createQuizPage/QuestionComp.js
@@ -41,60 +41,6 @@ export class QAentry extends Component {
   }
 }
 
-
-export class QAedit extends Component {
-
-  render() {
-    const quiz = this.props.quiz;
-    const key = this.props.qaID;
-    const qaType = this.props.qaType;
-    let ansDisplay=[];
-    let question = null;
-    
-    const displayOption = {
-                            "multipleChoice": {"caNumer": 1, "iaNumber": 3},
-                            "openEnded": {"caNumer": 4, "iaNumber": 0},
-                            "boolean": {"caNumer": 1, "iaNumber": 1},
-                          }
-
-    if (key !== null) {   // A question and answer object provided
-
-      console.log("key,",key)
-      const QA = quiz.QuestionsAndAnswers[key];
-      const correct_answers = QA.correct_answers;
-      const incorrect_answers = QA.incorrect_answers;
-      question = QA.question;
-
-      // Generate correct answer inputs      
-      for (let i=0; i<displayOption[qaType]["caNumer"]; i++) {
-        ansDisplay.push(<input type="text" placeholder="Correct Answer" className='CorrectAnswer' key={`'ca'${i}`} defaultValue={correct_answers[i]}/>);
-      }
-
-      // Generate wrong answer inputs
-      for (let i=0; i<displayOption[qaType]["iaNumber"]; i++) {
-        ansDisplay.push(<input type="text" placeholder="Wrong Answer" className='WrongAnswer' key={`'ia'${i}`} defaultValue={incorrect_answers[i]}/>);
-      }      
-
-    }
-
-    return (
-      <div>
-        <input type="text" placeholder="Question"  id = 'idQuestion' defaultValue={question}/>
-
-        <select name="type" id="idQuestionType" onChange={this.props.onChange} defaultValue={qaType}>
-          <option value="multipleChoice">Multiple Choice</option>
-          <option value="openEnded">Open Ended Question</option>
-          <option value="boolean">True or False</option>
-        </select>
-
-        {ansDisplay}
-
-        <button onClick={this.props.onClick}>Save</button>
-      </div>
-    );
-  }
-}
-
 export class QApreview extends Component {
 
   render() {
@@ -133,6 +79,58 @@ export class QApreview extends Component {
             onChange={this.props.onChange}
           />        
         </div>        
+      </div>
+    );
+  }
+}
+
+export class QAedit extends Component {
+
+  render() {
+    const quiz = this.props.quiz;
+    const key = this.props.qaID;
+    const qaType = this.props.qaType;
+    let ansDisplay=[];
+    let question = null;
+    
+    const displayOption = {
+                            "multipleChoice": {"caNumer": 1, "iaNumber": 3},
+                            "openEnded": {"caNumer": 4, "iaNumber": 0},
+                            "boolean": {"caNumer": 1, "iaNumber": 1},
+                          }
+
+    if (key !== null) {   // A question and answer object provided
+      const QA = quiz.QuestionsAndAnswers[key];
+      const correct_answers = QA.correct_answers;
+      const incorrect_answers = QA.incorrect_answers;
+      question = QA.question;
+
+      // Generate correct answer inputs      
+      for (let i=0; i<displayOption[qaType]["caNumer"]; i++) {
+        ansDisplay.push(<input type="text" placeholder="Correct Answer" className='CorrectAnswer' key={`'ca'${i}`} defaultValue={correct_answers[i]}/>);
+      }
+
+      // Generate wrong answer inputs
+      for (let i=0; i<displayOption[qaType]["iaNumber"]; i++) {
+        ansDisplay.push(<input type="text" placeholder="Wrong Answer" className='WrongAnswer' key={`'ia'${i}`} defaultValue={incorrect_answers[i]}/>);
+      }      
+
+      console.log("qaType", qaType);
+    }
+
+    return (
+      <div>
+        <input type="text" placeholder="Question"  id = 'idQuestion' defaultValue={question}/>
+
+        <select name="type" id="idQuestionType" onChange={this.props.onChange} value={qaType}>
+          <option value="multipleChoice">Multiple Choice</option>
+          <option value="openEnded">Open Ended Question</option>
+          <option value="boolean">True or False</option>
+        </select>
+
+        {ansDisplay}
+
+        <button onClick={this.props.onClick}>Save</button>
       </div>
     );
   }
