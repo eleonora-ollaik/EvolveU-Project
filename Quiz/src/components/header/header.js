25bcc9b187c78fb15659acb5a2a091a303437383
DwanW <kunvang@yahoo.com>
2020-07-17 11:46:50 -0600

    link createquiz page

--- Quiz/src/components/header/header.js
+++ Quiz/src/components/header/header.js
@@ -2,16 +2,13 @@ import React from 'react';
 
 import './header.css';
 import TakeQuiz from '../../pages/take-quiz/take-quiz';
-
-const CreateQuiz = () => (
-    <h1>createQuiz</h1>
-)
+import CreateQuizForm from '../../pages/createQuizPage/CreateQuizForm';
 
 const Header = ({currentPage, handleNavigation}) => (
     <div className='appHeader'>
         <button>logo</button>
         <div className='navbar'>
-            <button onClick={()=>handleNavigation(<CreateQuiz/>)}>create quiz</button>
+            <button onClick={()=>handleNavigation(<CreateQuizForm />)}>create quiz</button>
             <button onClick={()=>handleNavigation(<TakeQuiz/>)}>take quiz</button>
             <button onClick={()=>handleNavigation('createQuiz')}>quiz manager</button>
         </div>
