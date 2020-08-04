7b3a85c7a9b8dffb32e492262708872c06dfe0e1
Boris Kwok <bkwok123@gmail.com>
2020-08-04 14:02:53 -0600

    Added GetQuiz lambda function

--- Quiz/aws/lambda/ExecuteDBQuery/test_lambda_function.py
+++ Quiz/aws/lambda/ExecuteDBQuery/test_lambda_function.py
@@ -4,6 +4,7 @@
 # Step 3: pip install -r requirements.txt -t . (Update Pipfile dependencies to requirements.txt and install to current folder [.])
 # Step 4: Zip the package up and upload in AWS lambda console
 import pytest
+import copy
 
 from lambda_function import *
 from test_config import *
@@ -13,27 +14,14 @@ def test_lambda_function():
     #####################################
     # Check database query
     #####################################
+    # https://docs.aws.amazon.com/lambda/latest/dg/services-apigateway.html
     event = {}
     # event['query'] = "INSERT INTO theme (theme_name, theme_creation, theme_update) VALUES ('Test5', now(), now()) RETURNING *"
     # event['query'] = "UPDATE theme SET theme_name = 'History50', theme_update = now() WHERE theme_id = 2 RETURNING *"
     # event['query'] = "SELECT * FROM theme"
     # event['query'] = "DELETE FROM theme WHERE theme_id=29 RETURNING *"
-    # event['query'] = "SELECT * FROM quiz INNER JOIN theme ON quiz.theme_id = theme.theme_id"
-    # event['query'] = "SELECT * FROM \
-    #                     (SELECT * FROM quiz INNER JOIN theme ON quiz.theme_id = theme.theme_id) as ct1 \
-    #                         INNER JOIN question on ct1.quiz_id = question.quiz_id"
-    event['query'] ="SELECT * FROM \
-                        (SELECT * FROM quiz \
-                         INNER JOIN theme \
-                         ON quiz.theme_id = theme.theme_id) as ct1 \
-                    INNER JOIN \
-                        (SELECT * FROM \
-                            (SELECT * FROM question \
-                            INNER JOIN answer \
-                            ON question.question_id = answer.question_id) as ct3 \
-                        INNER JOIN questiontype \
-                        ON ct3.questiontype_id = questiontype.questiontype_id) as ct2 \
-                    ON ct1.quiz_id = ct2.quiz_id;"
+    event['query'] = "SELECT * FROM quiz INNER JOIN theme ON quiz.theme_id = theme.theme_id"
+
     event['haspayload'] = True
     context = ""
 
@@ -43,8 +31,21 @@ def test_lambda_function():
     os.environ['rds_port'] = config["rds_port"]
     os.environ['rds_username'] = config["rds_username"]    
 
+    # Testing successful case
     response = lambda_handler(event, context)
 
-    print(response["payload"][0])
-    print(response["body"])
     assert(response["statusCode"] == 200)
+    assert(response["body"] == "Query executed successfully")
+    assert('quiz_id' in response["payload"][0])
+    assert('quiz_name' in response["payload"][0])
+    assert('theme_id' in response["payload"][0])
+    assert('theme_name' in response["payload"][0])
+
+    # Testing failed case
+    event['query'] = ""
+    event['haspayload'] = True
+    context = ""
+
+    response = lambda_handler(event, context)
+    assert(response["statusCode"] == 400)
+    assert(response["body"] == "An error occurred")    
