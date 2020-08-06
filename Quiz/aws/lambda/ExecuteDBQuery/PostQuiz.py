33ad054edb765c334a93a0bd437a5ebf651bd111
Boris Kwok <bkwok123@gmail.com>
2020-08-06 11:32:39 -0600

    Revised Front End url to AWS urls

--- Quiz/aws/lambda/ExecuteDBQuery/PostQuiz.py
+++ Quiz/aws/lambda/ExecuteDBQuery/PostQuiz.py
@@ -2,78 +2,139 @@ import json
 import boto3
 
 # Define the client to interact with AWS Lambda
-client = boto3.client('lambda', region_name='ca-central-1')
+client = boto3.client('lambda')
 
 def lambda_handler(event, context):
-
-    # "quiz_name": "Name 1", 
-    # "theme_id": 1,
-    # "questions": [{"quiz_id": 1,
-    #                "question_category": "test category",
-    #                "questiontype_id": "test type",
-    #                "question_statement": "question 1",
-    #                "question_correct_entries": 0,
-    #                "question_wrong_entries": 0,
-    #                "answers": []
-    #                },                   
-    #               {"quiz_id": 2,
-    #                "question_category": "test category",
-    #                "questiontype_id": "test type",
-    #                "question_statement": "question 1",
-    #                "question_correct_entries": 0,
-    #                "question_wrong_entries": 2,
-    #                 "answers": []
-    #                }]
-    print("Before Try++++++++++++++++++++++++")
     try:
-        # query =f"SELECT * FROM \
-        #             (SELECT * \
-        #             FROM quiz \
-        #             INNER JOIN theme \
-        #             ON quiz.theme_id = theme.theme_id WHERE quiz_id = {event['quiz_id']}) as ct1 \
-        #         INNER JOIN  \
-        #             (SELECT * \
-        #             FROM \
-        #                 (SELECT * \
-        #                 FROM question \
-        #                 INNER JOIN answer \
-        #                 ON question.question_id = answer.question_id) as ct3 \
-        #             INNER JOIN questiontype \
-        #             ON ct3.questiontype_id = questiontype.questiontype_id) as ct2 \
-        #         ON ct1.quiz_id = ct2.quiz_id;"
+        ################################################### 
+        # Insert record into quiz table
+        ###################################################
+        quiz_name = event['quiz_name']
+        theme_id = event['theme_id']
+        query = f"INSERT INTO quiz (quiz_name, theme_id, quiz_creation, quiz_update) VALUES ('{quiz_name}', {theme_id}, now(), now()) RETURNING *;"  
 
-        query = "SELECT * FROM quiz"
-        print("query: ", query)
-
+        # Execute the SQL statement
         inputParams = {
             "query": query,
-            "haspayload": True   
+            "haspayload": False
         }
 
-        print("Before Invoke++++++++++++++++++++++++")
         response = client.invoke(
             FunctionName = 'arn:aws:lambda:ca-central-1:712789485255:function:ExecuteDBQuery',
             InvocationType = 'RequestResponse',
             Payload = json.dumps(inputParams)
         )
-        print("After Invoke++++++++++++++++++++++++")
 
-        responseFromChild = json.load(response["Payload"])
+        responseFromChild = json.load(response['Payload'])
+        
+        # Obtain new IDs from returning result after inserting new records    
+        payload = json.loads(responseFromChild["payload"])
+        
+        quiz_id = payload[0]['quiz_id']        
+        
+        
+        ################################################### 
+        # Insert record into Question table
+        ################################################### 
+        questions = event['questions']
+        questionSQL = "INSERT INTO question (quiz_id, question_category, \
+    				   questiontype_id, question_statement, \
+    				   question_correct_entries, question_wrong_entries, \
+    				   question_creation, question_update) "
+        questionValues = "VALUES"
+    
+        for q in questions:
+            questionValues = f"{questionValues} ({quiz_id}, '{q['question_category']}', \
+                               {q['questiontype_id']}, '{q['question_statement']}', \
+                               {q['question_correct_entries']}, {q['question_wrong_entries']}, \
+                               now(), now()),"
+    
+        # Remove the last character and append SQL returning keyword for creating answers with returned question id
+        questionValues = questionValues[:-1] + " RETURNING *;"    
+        query = questionSQL + questionValues
+    
+        # Execute the SQL statement
+        inputParams = {
+            "query": query,
+            "haspayload": True
+        }
+
+        response = client.invoke(
+            FunctionName = 'arn:aws:lambda:ca-central-1:712789485255:function:ExecuteDBQuery',
+            InvocationType = 'RequestResponse',
+            Payload = json.dumps(inputParams)
+        )
+
+        responseFromChild = json.load(response['Payload'])        
+        
+        # Obtain new IDs from returning result after inserting new records
+        payload = json.loads(responseFromChild["payload"])
+        question_ids = [x['question_id'] for x in payload]
+        
+        ################################################### 
+        # Insert record into Answer table
+        ###################################################
+        answerSQL = "INSERT INTO answer (question_id, \
+    				 answer_is_correct, answer_statement, \
+    				 answer_creation, answer_update) "
+        answersValues = "VALUES"
+        i = 0
+        for q in questions:
+            answers = q['answers']
+            for a in answers:
+                answersValues = f"{answersValues} ({question_ids[i]}, \
+                                  {a['answer_is_correct']}, '{a['answer_statement']}', \
+                                  now(), now()),"
+            i = i+1
+            
+        # Remove the last character and append SQL returning keyword
+        answersValues = answersValues[:-1] + " RETURNING *;"    
+        query = answerSQL + answersValues
+    
+        # Execute the SQL statement
+        inputParams = {
+            "query": query,
+            "haspayload": True
+        }
+
+        response = client.invoke(
+            FunctionName = 'arn:aws:lambda:ca-central-1:712789485255:function:ExecuteDBQuery',
+            InvocationType = 'RequestResponse',
+            Payload = json.dumps(inputParams)
+        )
+
+        responseFromChild = json.load(response['Payload'])
+    
+        # Obtain new IDs from returning result after inserting new records
+        payload = json.loads(responseFromChild["payload"])
+        answer_ids = [x['answer_id'] for x in payload]
+    
+        # Update json with newly created ids
+        event['quiz_id'] = quiz_id
+        i=0
+        j=0
+        for q in questions:
+            event['questions'][i]['question_id'] = question_ids[i]        
+            k=0
+            for a in answers:
+                event['questions'][i]['answers'][k]['answer_id'] = answer_ids[j]
+                j=j+1
+                k=k+1
+            i=i+1
+        
         if responseFromChild['statusCode'] > 200:
             raise
 
-        print("Before return 200")
         return {
             'statusCode': 200,
-            'body': "Query Executed Successfully",
-            'payload': json.loads(responseFromChild["payload"])
+            'body': 'Query Executed Successful',
+            'payload': event
         }
 
-    except Exception as e:
+    except:
 
-        print("Before return 400", e)
         return {
             'statusCode': 400,
-            'body': json.dumps('An error occurred'),
+            'body': 'An error occurred',
             'payload': {}
         }
